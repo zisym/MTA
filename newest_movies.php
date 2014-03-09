7e866442204a64e4351a1255b356408b824e5efa
remoteriver <remoteriver@gmail.com>
2014-03-09 14:56:08 -0400

    update video list

---
+++ newest_movies.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+
+ <table cellspacing="5" border="0">
+          
+  <tr>
+    <td rowspan=2><img src="image/1.jpg" width="356" height="531"></td>
+    <td><img src="image/download (1).jpg" width="178" height="263"></td>
+    <td><img src="image/download (1).jpg" width="178" height="263"></td>
+  </tr>
+  <tr>
+    <td><img src="image/download (2).jpg" width="178" height="263"></td>
+    <td><img src="image/download (2).jpg" width="178" height="263"></td>
+    
+  </tr>
+</table>
+</body>
+</html>
